8fc0d07779648b0bd842318fe09b2d74f4e408d4
Peterdinis <peterdinis0@pobox.sk>
2022-06-18 14:37:00 +0200

    Adding new profile page

--- src/pages/auth/profile/ProfileHeader.tsx
+++ src/pages/auth/profile/ProfileHeader.tsx
@@ -3,17 +3,26 @@ import "./Profile.css"
 function ProfileHeader() {
   return (
     <div id="profile-upper">
-        <div id="profile-banner-image">
-            <img src="https://imagizer.imageshack.com/img921/9628/VIaL8H.jpg" alt="Banner image" />
-        </div>
-
-        <div id="profile-d">
-            <div id="profile-pic">
-            <img src="https://imagizer.imageshack.com/img921/3072/rqkhIb.jpg" />
+    <div id="profile-banner-image">
+      <img src="https://imagizer.imageshack.com/img921/9628/VIaL8H.jpg" alt="Banner image" />
+    </div>
+    <div id="profile-d">
+      <div id="profile-pic">
+        <img src="https://imagizer.imageshack.com/img921/3072/rqkhIb.jpg" />
       </div>
       <div id="u-name">Himalaya Singh</div>
-            </div>
+      <div className="tb" id="m-btns">
+        <div className="td">
+          <div className="m-btn"><i className="material-icons">format_list_bulleted</i><span>Activity log</span></div>
         </div>
+        <div className="td">
+          <div className="m-btn"><i className="material-icons">lock</i><span>Privacy</span></div>
+        </div>
+      </div>
+      <div id="edit-profile"><i className="material-icons">camera_alt</i></div>
+    </div>
+    <div id="black-grd"></div>
+  </div>
   )
 }
 
